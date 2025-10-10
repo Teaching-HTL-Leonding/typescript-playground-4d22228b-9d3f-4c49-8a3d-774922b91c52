7918dc18ae9b5a9c019bd6c0be43dd7334df8c13
Rainer Stropek <rainer@timecockpit.com>
2025-10-10 09:33:38 +0200

    Saved in TypeScript Playground at 2025-10-10T07:33:37.872Z

--- flowers_basics/index.ts
+++ flowers_basics/index.ts
@@ -1,4 +1,6 @@
-// <<< ADD CONSTANTS HERE (if you need them)
+const BLUMEEINFACHDURCHMESSER = 80
+const BLUMESCHWERDURCHMESSER = 70
+const KREISX = 300
 
 function setup() {
   createCanvas(1000, 1000)
@@ -15,21 +17,21 @@ function setup() {
 
   fill("lightgreen")
   strokeWeight(1)
-  circle(750, 200, 80)
-  circle(700, 250, 80)
-  circle(650, 200, 80)
-  circle(700, 150, 80)
+  circle(750, 200, BLUMEEINFACHDURCHMESSER)
+  circle(700, 250, BLUMEEINFACHDURCHMESSER)
+  circle(650, 200, BLUMEEINFACHDURCHMESSER)
+  circle(700, 150, BLUMEEINFACHDURCHMESSER)
 
   fill("yellow")
   noStroke()
   circle(700, 200, 65)
 
   fill("lightgreen")
-  circle(300 + 15.5, 200 - 47.5, 70)
-  circle(350, 200, 70)
-  circle(300 + 15.5, 247.5, 70)
-  circle(300 - 44, 200 - 31.9, 70)
-  circle(300 - 44, 200 + 31.9, 70)
+  circle(KREISX + 15.5, 200 - 47.5, BLUMESCHWERDURCHMESSER)
+  circle(350, 200, BLUMESCHWERDURCHMESSER)
+  circle(KREISX + 15.5, 247.5, BLUMESCHWERDURCHMESSER)
+  circle(KREISX - 44, 200 - 31.9, BLUMESCHWERDURCHMESSER)
+  circle(KREISX - 44, 200 + 31.9, BLUMESCHWERDURCHMESSER)
 
   fill("yellow")
   circle(300, 200, 65)
