9a4a008a9e3d626a4d5c7390472794cd19f6bd9e
Rainer Stropek <rainer@timecockpit.com>
2025-11-14 09:52:13 +0100

    Saved in TypeScript Playground at 2025-11-14T08:52:13.437Z

--- color_areas/index.ts
+++ color_areas/index.ts
@@ -6,7 +6,7 @@ function setup() {
     rect(0, 0, width / 3 , height)
 
     fill ("green")
-    rect(width / 3, 0 width / 3 , height)
+    rect(width / 3, 0 ,width / 3 , height)
 
     fill("blue")
     rect(width / 3 * 2, 0, width / 3, height)
@@ -14,15 +14,26 @@ function setup() {
     fill("black")
     rect(0, 150 , width , 50)
 
+    // <<< Add your code here
 }
 
-let message: string
-
-// <<stringthe function `moveClicked` with the required code here
+// <<< Add the function `moveClicked` with the required code here
 function mouseClicked() {
+
+    fill("black")
+    rect(0, 150, width, 50)
+
+    noStroke()
+    fill("white")
+    textSize(20)
+    textAlign(CENTER, CENTER)
+
+    let message: string
 
     if  (mouseX < width / 3) {
     message = "yellow" }
+    else if (mouseX < width / 3 * 2 ) {
+    message = "green"}
+    else { message = "blue"}
 
     }
-}
