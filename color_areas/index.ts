f90210f3a0a7c526e8141ffb1f4863ee0eb310ef
Rainer Stropek <rainer@timecockpit.com>
2025-11-14 09:30:59 +0100

    Saved in TypeScript Playground at 2025-11-14T08:30:58.895Z

--- color_areas/index.ts
+++ color_areas/index.ts
@@ -2,14 +2,24 @@ function setup() {
     createCanvas(400, 200);
     background("black");
     noStroke();
+    fill("orange")
+    rect(0, 0, width / 3 , height)
 
-    // <<< Add your code here
+    fill ("green")
+    rect(width / 3, 0 width / 3 , height)
+
+    fill("blue")
+    rect(width / 3 * 2, 0, width / 3, height)
+
+    fill("black")
+    rect(0, 150 , width , 50)
+
 }
 
-// <<< Add the function `moveClicked` with the required code here
-function mouseClicked() {
+let message: string
 
-    let message: string
+// <<stringthe function `moveClicked` with the required code here
+function mouseClicked() {
 
     if  (mouseX < width / 3) {
     message = "yellow" }
