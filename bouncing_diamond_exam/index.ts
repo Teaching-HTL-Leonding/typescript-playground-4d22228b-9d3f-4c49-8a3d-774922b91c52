4b3e15b288fb50d9296fbf2281903e154cc8c0b5
Rainer Stropek <rainer@timecockpit.com>
2025-12-05 09:06:07 +0100

    Saved in TypeScript Playground at 2025-12-05T08:06:07.227Z

---
+++ bouncing_diamond_exam/index.ts
@@ -0,0 +1,42 @@
+function setup() {
+    createCanvas(300, 300);
+}
+
+let DiamondX = 100
+let DiamondY = 50
+
+let directionX = 2;
+let directionY = 2;
+
+
+
+function draw() {
+    background("gold");
+    stroke("white")
+    strokeWeight(5)
+    line(DiamondX + 50, DiamondY - 50, DiamondX + 75, DiamondY - 25)
+    line(DiamondX + 50, DiamondY - 50, DiamondX + 25, DiamondY - 25)
+    line(DiamondX + 75, DiamondY - 25, DiamondX + 50, DiamondY)
+    line(DiamondX + 25, DiamondY - 25, DiamondX + 50, DiamondY)
+
+    DiamondX += directionX
+    DiamondY += directionY
+
+    noStroke();
+    fill("black");
+    textSize(20);
+    textStyle(BOLD);
+    textAlign(CENTER, CENTER);
+
+    if (DiamondX <= 0 - 25 || DiamondX >= height - 75) {
+        directionX *= -1;
+    }
+    if (DiamondY <= 0 + 50 || DiamondY >= 300) {
+        directionY *= -1;
+    }
+
+
+    text(10, DiamondX + 50, DiamondY - 25
+    )
+
+}
