fb35dbc1849cb4e9f70b5db359f7d676e8a7806a
Rainer Stropek <rainer@timecockpit.com>
2025-12-12 10:15:36 +0100

    Saved in TypeScript Playground at 2025-12-12T09:15:36.554Z

---
+++ grid/index.ts
@@ -0,0 +1,34 @@
+function setup() {
+    // We have a square canvas. The side length is defined by the constant SIZE.
+    const SIZE = 400;
+    // Your job is to draw a grid on the canvas. The distance between the grid lines is defined by the constant GRID.
+    const GRID = 25;
+
+    createCanvas(SIZE, SIZE);
+    background("black");
+
+    strokeWeight(0.25);
+    stroke("lightgray");
+
+    // Draw a grid by using a while loop.
+    
+    // Part 1: Draw vertical lines
+    let i = GRID;
+    while (i < SIZE) {
+        line(i, 0, i, SIZE);
+        i += GRID;
+    }
+
+        i = GRID;
+    while (i < SIZE) {
+        line(GRID * (i / GRID), i, SIZE - GRID, i);
+        i += GRID;
+    }
+
+
+    // Part 2: Draw horizontal lines
+    
+    for ( i = GRID; i < SIZE; i += GRID) {
+        line(0, i, SIZE, i);
+    }
+}
