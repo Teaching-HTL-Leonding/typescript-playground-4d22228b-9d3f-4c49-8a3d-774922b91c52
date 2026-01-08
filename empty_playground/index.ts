ef70678c92766d368faea45f07cce4e5b038975a
Rainer Stropek <rainer@timecockpit.com>
2026-01-08 13:32:18 +0100

    Saved in TypeScript Playground at 2026-01-08T12:32:18.107Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,20 +1,28 @@
 function setup() {
 
     createCanvas(500, 500)
-    background("red")
+    background("white")
+    const centerY: number[] = [161, 180, 210, 250,]
+    const centerX: number[] = [161, 180, 210, 250,]
+    const Durchmesser: number[] = [20, 40, 60, 80, 100]
 
-    for (let i = 1; i < 10; i += 2) {
-        fill("black")
-        rect(50 * i, 50, 40, 40)
+    console.log("vor push", Durchmesser)
+    centerX.push(300)
+    centerY.push(300)
+    console.log("nach push", Durchmesser)
+
+    centerX.splice(0, 3)
+    centerY.splice(0, 3)
+    Durchmesser.splice(0, 3)
+
+    fill("red")
+    noStroke()
+    for (let i = 0; i < centerX.length; i++) {
+        circle(centerX[i], centerY[i], Durchmesser[i])
     }
 
-    let j = 1
 
-    while(j < 10){
-        fill("black")
-        rect(50 * j, 200, 40, 40)
-        j += 2
-    }
 }
 
-
+function draw() {
+}
