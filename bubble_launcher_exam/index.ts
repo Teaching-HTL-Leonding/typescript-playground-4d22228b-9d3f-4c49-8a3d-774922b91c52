37cde46fa421093aaf35795cd36d98ceb11677d2
Rainer Stropek <rainer@timecockpit.com>
2026-01-16 09:26:24 +0100

    Saved in TypeScript Playground at 2026-01-16T08:26:23.823Z

--- bubble_launcher_exam/index.ts
+++ bubble_launcher_exam/index.ts
@@ -12,26 +12,26 @@ function setup() {
 }
 
 function draw() {
-
-
     background("white");
-
     fill('lightgray');
     stroke('darkgray');
     rect(0, 0, DIAM, height);
 
-
     fill('yellow');
     stroke('gold');
+
+    circle(10, mouseY, DIAM)
     for (let i = 0; i < circleX.length; i++) {
         push()
         circle(circleX[i], circleY[i], DIAM)
         circleX[i] += SPEED * direction[i];
-    
-    
+        if (circleX[i] - RADI < 0 || circleX[i] + RADI > width) {
+            direction[i] *= -1;
+            circleX[i] = max(RADI, min(width - RADI, circleX[i]));
+        }
         pop()
     }
-    }
+}
 
 function mouseClicked() {
 
