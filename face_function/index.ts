6c76c4404ffc7fed87598ffb62653ce69271daf3
Rainer Stropek <rainer@timecockpit.com>
2026-02-27 08:10:52 +0100

    Saved in TypeScript Playground at 2026-02-27T07:10:51.679Z

---
+++ face_function/index.ts
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(500, 500);
+  angleMode(DEGREES);
+
+  // Function CALL
+  // +------------------------------ Function name
+  // |  +--------------------------- Function parameter
+  // v  v
+  smile(true);
+}
+
+// Function DEFINITION
+//       +-------------------------- Function name
+//       |     +-------------------- Parameter name
+//       |     |      +------------- Parameter type
+//       v     v      v
+function smile(happy: boolean) {
+  // <<< Add your code here
+}
